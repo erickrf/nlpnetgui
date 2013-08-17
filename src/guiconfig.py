de23d91e88158be5917dfc93215b4285dcec23ac
Erick Rocha Fonseca <erickrfonseca@gmail.com>
2013-08-17 11:49:12 -0300

    configuration file

---
+++ src/guiconfig.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+'''
+Configuration data for the NLPNET GUI
+
+@author: Erick Fonseca
+'''
+
+from os.path import join
+
+FILE_DIR = 'files'
+
+SPLASH = join(FILE_DIR, 'splash.bmp')
+DESCRIPTION = join(FILE_DIR, 'desc.txt')
+ICON=  join(FILE_DIR, 'icon.ico')
+
